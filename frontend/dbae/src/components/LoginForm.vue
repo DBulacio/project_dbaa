0fb95d0b35af02c31b71e768984d4725d4f37bee
TunaNigiri <antuelbolson@gmail.com>
2023-09-09 16:36:36 -0300

    El loginForm funcionaaaa!!!!

---
+++ frontend/dbae/src/components/LoginForm.vue
@@ -0,0 +1,54 @@
+<template>
+   <div class="container"> 
+            <input v-model="this.inputName" type="text">Name:
+            {{ this.inputName }}
+            <input v-model="this.inputPassword" type="password">Password:
+            <button :disabled="this.disableButton" @click="login()">{{loginButton}}</button>
+        
+    </div>
+
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                password: "1234",
+                userName: "Alberto",
+                loginButton: "Login",
+                disableButton: false,
+                inputName: "",
+                inputPassword: ""
+
+            }
+        },
+        methods: {
+        login() {
+            
+            this.disableButton = true
+            if (this.inputName === this.userName && this.inputPassword === this.password){
+                console.log("Bienvenido ahre")
+                this.loginButton = "Loading"
+            }
+            else {
+                console.log("Dale wahcin...")
+                this.disableButton = false
+                this.inputName = ""
+                this.inputPassword = ""
+            }
+            
+        }
+    }
+}
+    
+
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  margin: 5px;
+}
+
+</style>
---
+++ frontend/dbae/src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <LoginForm />
+  </div>
+</template>
+
+<script>
+  import { defineAsyncComponent} from 'vue';
+  export default {
+    components: {
+      LoginForm: defineAsyncComponent(() => import ('../components/LoginForm.vue'))
+    }
+  }
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .login {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    margin: 5px;
+  }
+}
+
+</style>
